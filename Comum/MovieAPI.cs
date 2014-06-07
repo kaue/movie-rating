e5255a1931e32dff1f7e96cf104d68591c68dd6c
Kauê Gimenes <kaue@dot.net>
2014-06-07 19:05:44 -0300

    Resolver problema solution VS

---
+++ Comum/MovieAPI.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Comum
+{
+    public class MovieAPI
+    {
+        private static int votosMinimos = 200;
+
+        public static string GerarUrlApi(int lPage)
+        {
+            return string.Format("https://api.themoviedb.org/3/discover/movie?api_key=e6797a9cbed39809f952f4245d0cca9b&vote_count.gte={0}&page={1}&include_adult=false", votosMinimos, lPage);
+        }
+
+        public static int ObterNumeroPaginas()
+        {
+            string apiResult;
+            JObject jObj;
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            //Obter numero de paginas.
+            apiResult = Core.HttpGet(GerarUrlApi(1));
+            jObj = JObject.Parse(apiResult);
+            int numeroPaginas = int.Parse(((JValue)jObj["total_pages"]).ToString());
+            return numeroPaginas;
+        }
+
+        public static Entidade.Filme ObterFilmeAleatorio(int lPaginas)
+        {
+            Entidade.Filme filme = new Entidade.Filme();
+            string apiResult;
+            JObject jObj;
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            //Obter Filmes
+            apiResult = Core.HttpGet(GerarUrlApi(rnd.Next(1, lPaginas)));
+            jObj = JObject.Parse(apiResult);
+            int totalFilmesPagina = jObj["results"].Count();
+            var filmeSelecionado = jObj["results"][rnd.Next(0, totalFilmesPagina)];
+            filme.Codigo = int.Parse(filmeSelecionado["id"].ToString());
+            filme.Nome = filmeSelecionado["title"].ToString();
+            filme.Capa = filmeSelecionado["poster_path"].ToString();
+            return filme;
+        }
+
+    }
+}
---
+++ NG/Voto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NG
+{
+    public class Voto
+    {
+        public static void IncluirVoto(Entidade.Voto lVoto){
+            //Incluir filme caso nao exista no banco
+            if (!NG.Filme.ExisteFilme(lVoto.Filme))
+                NG.Filme.IncluirFilme(lVoto.Filme);
+
+            DB.Voto dbVoto = new DB.Voto();
+            dbVoto.IncluirVoto(lVoto);
+        }
+<<<<<<< HEAD
+
+       
+=======
+>>>>>>> 28d8ae1e79940b7d2aa49d266638a6bc4235f4d8
+    }
+}
