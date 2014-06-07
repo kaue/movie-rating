52e6d91cedec3dc8f083376984b1810cd7a0f77c
Kauê Gimenes <kaue@dot.net>
2014-06-07 17:31:51 -0300

    Adicionar camada de Negocio e NG Emocao

---
+++ NG/Emocao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NG
+{
+    public class Emocao
+    {
+        public static Entidade.Emocao ObterEmocaoAleatoria()
+        {
+            DB.Emocao dbEmocao = new DB.Emocao();
+            List<Entidade.Emocao> listaEmocao = dbEmocao.ListarEmocao();
+            Random rnd = new Random();
+            return listaEmocao[rnd.Next(listaEmocao.Count)];
+        }
+
+        public static List<Entidade.Emocao> ListarEmocaoPorFilme(int lCodigoFilme)
+        {
+            DB.Emocao dbEmocao = new DB.Emocao();
+            List<Entidade.Emocao> listaEmocao = dbEmocao.ListarEmocaoPorFilme(lCodigoFilme);
+            return listaEmocao;
+        }
+    }
+}
