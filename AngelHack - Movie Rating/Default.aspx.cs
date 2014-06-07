47fdf53468a12c2d74a13f5f2e05068f796bc731
Kauê Gimenes <kaue@dot.net>
2014-06-07 16:29:44 -0300

    Implementar API TheMovieDB

--- AngelHack - Movie Rating/Default.aspx.cs
+++ AngelHack - Movie Rating/Default.aspx.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace AngelHack___Movie_Rating
+namespace Web
 {
     public partial class Default : System.Web.UI.Page
     {
