1d12fb081d85983b9f19dded740ccc9cf08c24c5
Kauê Gimenes <kaue@dot.net>
2014-06-07 16:15:26 -0300

    Adicionar Entidades Filme, Emocao e Voto no projeto.

---
+++ Entidade/Emocao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidade
+{
+    public class Emocao
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public int Total { get; set; }
+        public int TotalGeralPositivo { get; set; }
+        public int TotalGeralNegativo { get; set; }
+    }
+}
---
+++ Entidade/Voto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidade
+{
+    public class Voto
+    {
+        public int CodigoFilme { get; set; }
+        public int CodigoEmocao { get; set; }
+        public int Valor { get; set; }
+        public DateTime Data { get; set; }
+        public Entidade.Filme Filme { get; set; }
+        public Entidade.Emocao Emocao { get; set; }
+    }
+}
